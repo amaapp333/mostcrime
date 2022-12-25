cabae74fbd2e97dfdac6ac5d114b31e3f73c82a7
amaapp333 <118077998+amaapp333@users.noreply.github.com>
2022-12-25 23:55:48 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,14 @@ import Link from 'next/link'
 
 export default function IndexPage() {
   return (
+    <head>
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+
+    </head>
     <div>
-      Hello World. <Link href="/about">About</Link>
+     <h1>Hello World.</h1>
+
+    <Link href="/about">About</Link>
     </div>
   )
 }
